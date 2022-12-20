b7822369c1329d74a3bbe8c4fe243ad6cb83079b
Don Gray <don@donaldegray.com>
2022-12-20 14:21:59 -0500

    initial work on building tests

---
+++ _tests_/buildForm-tests.js
@@ -0,0 +1,15 @@
+// test for building the YAW form
+//
+// the tests for building a row (div actually)
+
+'use strict'; // just because I think it's a good idea
+
+test ('make sure we have a complete row of data', () =>  {
+
+  require('../buildForm.js'); 
+
+});
+
+
+
+
