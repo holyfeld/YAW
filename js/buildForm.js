ddf66b5f9e8cf5f0be85d0939f2601d8d0f70b19
Don Gray <don@donaldegray.com>
2022-12-20 14:23:53 -0500

    starting work on tests

---
+++ js/buildForm.js
@@ -0,0 +1,18 @@
+// 
+// javascript code to build a form for getting information about the users org culture
+//
+'use strict';
+
+const formData = [
+    {topic:"Orientation",
+     text: "something useful here",
+     hurt: "Power Oriented",
+     cripple: "Rule Oriented",
+     boost: "Performance Oriented"}];
+
+ let dataRow = formData[0];
+     
+function buildDiv (dataRow) {
+// take a row from the data array and build division of the form
+
+}
